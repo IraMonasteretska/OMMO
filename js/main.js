e4cc289a0ef4b03da32098caa78390545384d26c
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2023-03-02 17:01:45 +0200

    Add  video block

--- js/main.js
+++ js/main.js
@@ -21,35 +21,63 @@ $(function () {
   var windowWidth = $(document).width(),
     containerWidth = $('.container').width(),
     paddingContainer = ((windowWidth - containerWidth) / 2)
-  $('.testimonials').css('padding-left', paddingContainer);
+  $('.testimonials__bottom').css('padding-left', paddingContainer);
 
   $(window).resize(function () {
     var windowWidth = $(document).width(),
       containerWidth = $('.container').width(),
       paddingContainer = ((windowWidth - containerWidth) / 2)
-    $('.testimonials').css('padding-left', paddingContainer);
+    $('.testimonials__bottom').css('padding-left', paddingContainer);
   });
 
   // TESTIMONIALS SLIDER --------------------------- //
-  var swiper = new Swiper(".testimonials-slider", {
+  var swiper = new Swiper(".testimonials__slider", {
     slidesPerView: 1,
     spaceBetween: 20,
+    speed: 800,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
     breakpoints: {
-      576: {
+
+      375: {
         slidesPerView: 1.5,
         spaceBetween: 20,
       },
 
-      1199: {
+      800: {
         slidesPerView: 2.5,
         spaceBetween: 20,
       },
 
     },
+  });
+
+  $('.video-play').on('click', function() {
+    $(this).hide();
+    $('.video-poster').hide();
+  })
+
+
+
+  // POPUP ---------------------------------------------- //
+
+  $('.popup_btn').on('click', function () {
+    var indexPopup = $(this).attr('data-popup');
+    $('.popup__window').addClass('hide');
+    $('.' + indexPopup).removeClass('hide');
+    return false;
+});
+
+  $('.popup__close').on('click', function() {
+    $('.popup__window').addClass('hide');
+  });
+
+  $('.popup__form').find('input').each( function() {
+    if($(this).text == '') {
+      console.log('good');
+    }
   });
 
   // AOS AIMATE -------------------------------------- //
